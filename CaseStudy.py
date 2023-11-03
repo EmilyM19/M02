3a174d72b5dc39a8fec816de6def87b5bf2f620e
Emily Mullins <emullins19@ivytech.edu>
2023-11-02 21:07:02 -0400

    Modified M02 Case Study

--- CaseStudy.py
+++ CaseStudy.py
@@ -12,7 +12,6 @@ the quit phrase 'ZZZ'.
 quit = "ZZZ"
 lName = input("Enter the student's last name, or 'ZZZ' to quit: ")
 while lName != quit:
-
     fName = input("Enter the student's first name: ")
     studentGPA = float(input("Enter the student's GPA: "))
     if studentGPA >= 3.5:
@@ -20,6 +19,5 @@ while lName != quit:
     elif studentGPA >= 3.25:
         print(fName + " " + lName + " has made the Honor Roll.")
     lName = input("Enter the student's last name, or 'ZZZ' to quit: ")
-
 print("You have ended the program.")
 exit()

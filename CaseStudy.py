92c0bff1f71a6dcdb66c89ecccb6388bd2f3e594
Emily Mullins <emullins19@ivytech.edu>
2023-11-04 16:49:32 -0400

    Changed while statement

--- CaseStudy.py
+++ CaseStudy.py
@@ -10,14 +10,16 @@ the quit phrase 'ZZZ'.
 """
 
 quit = "ZZZ"
-lName = input("Enter the student's last name, or 'ZZZ' to quit: ")
-while lName != quit:
-    fName = input("Enter the student's first name: ")
-    studentGPA = float(input("Enter the student's GPA: "))
-    if studentGPA >= 3.5:
-        print(fName + " " + lName + " has made the Dean's List.")
-    elif studentGPA >= 3.25:
-        print(fName + " " + lName + " has made the Honor Roll.")
+
+while True:
     lName = input("Enter the student's last name, or 'ZZZ' to quit: ")
-print("You have ended the program.")
-exit()
+    if lName == quit:
+        print("You have ended the program")
+        break
+    else:
+        fName = input("Enter the student's first name: ")
+        GPA = float(input("Enter the student's GPA: "))
+        if GPA >= 3.5:
+            print(fName + " " + lName + " has made the Dean's List.")
+        elif GPA >= 3.25: 
+            print(fName + " " + lName + " has made the Honor Roll.")
